7933ddb2340bc6a6ca8217680171b1079efdb7f8
dimkahare <dimkahare@gmail.com>
2019-02-15 16:16:50 +0400

    Initialize scene

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'webgl-learn.js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'web'),
+        compress: true,
+        index: 'index.html',
+        open: true,
+        port: 9000
+    }
+};
